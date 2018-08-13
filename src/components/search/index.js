8718330d0f0457e3a3e4d380277dee5a483ef2d4
Mona Lisacunduts <yusrilsabir22@gmail.com>
2018-08-13 18:01:37 +0700

    changer search book api

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -19,26 +19,28 @@ class Searching extends React.Component {
          
         return (
             <div className="container">
-                <SearchBar onFetchBookApi={this.props.onFetchBookApi}/>
+            <Link to="/dashboard" className="btn btn-info">Dashboard</Link>
                 <div className="card">
                     <div className="card-header">
-                        <Link to="/dashboard">Dashboard</Link>
+                <SearchBar onFetchBookApi={this.props.onFetchBookApi}/>
                     </div>
                     <div className="card-body">
                        {
                            this.props.book.receiveData !== undefined ?
                            this.props.book.receiveData.map((data, index) => {
+                               let genre = data.genre.join(', ');                               
+                               console.log()
                                 return (
                                     <div className="card" key={index}>
                                         <div className="card-body">
                                             <h4 className="card-">{data.title}</h4>
                                             <div className="row">
                                                 <div className="col-md-4">Short History</div>
-                                                <div className="col-md-8">{data.short_history}</div>                            
+                                                <div className="col-md-8 text-black-50">{data.short_history}</div>                            
                                             </div>
                                             <div className="row">
                                                 <div className="col-md-4">Genre</div>
-                                                <div className="col-md-8"><i className="text-muted">{data.genre}</i></div>
+                                                <div className="col-md-8 text-black-50"><i className="text-muted">{genre}</i></div>
                                             </div>
                                         </div>
                                     </div>
